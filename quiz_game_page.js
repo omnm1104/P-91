4e83cbd12b58d2ff0c39e2132595f646c8e2677c
omnm1104 <82947324+omnm1104@users.noreply.github.com>
2021-05-12 18:14:26 +0530

    Add files via upload

---
+++ quiz_game_page.js
@@ -0,0 +1,12 @@
+function send(){
+    number1 = document.getElementById("number_1").value;
+    number2 = document.getElementById("number_2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+    question_number = "<h4>" + number1 + "*" + number2 + "</h4>";
+    input_box = "<br>Answer:- <input id = 'answer_input' type = 'number'>";
+    check_button = "<br><br><button class = 'btn btn-info' onclick = 'check()'>Check</button>"
+    row = question_number + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+}
